f99c49e0dcc138f0b3012f7cadb4e06b7d0cbe31
MauricioMacedoVillarnobo <mauricio.macedo.villarnobo@gmail.com>
2026-03-15 02:59:00 -0300

    Isort/Ruff ajusts

--- app/engines/translator_engine.py
+++ app/engines/translator_engine.py
@@ -3,6 +3,7 @@ from typing import Protocol
 
 class TranslatorEngine(Protocol):
     """Class that setups the interface for translator engines"""
+
     def translate(self, text: str, source_language: str, target_language: str) -> str:
         """
         Receives a text in source_language and converts it into
